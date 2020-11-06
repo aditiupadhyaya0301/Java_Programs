45597a3cbfea704779ed0532a89b66da2c9007ac
aditiupadhyaya0301 <aditi.upadhyaya03@gmail.com>
2020-11-06 21:39:38 +0530

    Daily Employee Wage

---
+++ DailyEmpWage.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @authorAditiUpadhyay
+ * Functions:Static Program Understandings
+ *
+ */
+class DailyEmpWage {
+public static void main(String args[]) {
+int wageperhr=20;
+int hrs=8;
+int wages=wageperhr*hrs;
+int presence=(int)Math.floor(Math.random()*10)%2;
+if(presence==0){
+System.out.println("Employee is absent");
+}
+if(presence==1){
+System.out.println("Employee is present");
+System.out.println("Wages:"+ wages);
+}
+
+}
+}
